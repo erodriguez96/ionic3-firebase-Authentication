ef5b8fa10d007c61b63adba2ae63a2b483d02d68
erodriguez96 <eduardo.rdgz@hotmail.com>
2019-04-04 17:57:36 +0100

    logout button added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Alert, NavController} from 'ionic-angular';
+import { AuthenticationProvider } from "../../providers/authentication/authentication";
+import {LoginPage} from "../login/login";
 
 @Component({
   selector: 'page-home',
@@ -7,7 +9,22 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public authProvider: AuthenticationProvider) {
+
+  }
+
+  logUserOut(){
+    let data = this.authProvider.logoutUser();
+
+    if(data){
+
+      this.navCtrl.setRoot(LoginPage);
+
+    }else{
+
+      alert("Failure");
+
+    }
 
   }
 
